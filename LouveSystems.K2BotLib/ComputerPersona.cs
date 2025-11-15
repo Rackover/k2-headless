f32a86c15faa27e8c06806dedff8ac7c9e2b48ad
Louvenarde <louve@louve.systems>
2025-11-15 01:23:41 +0100

    K2BotLib is its own thing now

--- LouveSystems.K2BotLib/ComputerPersona.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LouveSystems.K2.BotLib
-{
-    public struct ComputerPersona
-    {
-        public string name;
-        public byte gender;
-        public byte? factionIndexFilter;
-    }
-}
